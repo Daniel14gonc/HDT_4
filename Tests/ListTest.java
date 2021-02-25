5fab2e2ff6e412cd16431057b03032ba8cdb95e8
Daniel Gonzalez <gon20293@uvg.edu.gt>
2021-02-24 21:06:41 -0600

    Creacion de test de lista

---
+++ Tests/ListTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+public class ListTest {
+
+    @org.junit.Test
+    public void size() {
+    }
+
+    @org.junit.Test
+    public void isEmpty() {
+    }
+
+    @org.junit.Test
+    public void add() {
+    }
+
+    @org.junit.Test
+    public void getLast() {
+    }
+
+    @org.junit.Test
+    public void remove() {
+    }
+}
